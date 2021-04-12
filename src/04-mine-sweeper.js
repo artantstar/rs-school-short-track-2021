6f23cda381d698bca50a0e7d3a79326ac8f752ca
artantstar <artantstar@gmail.com>
2021-04-13 02:51:31 +0300

    feat: complete task

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,60 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const newMatrix = new Array(matrix.length).fill(0).map(() => new Array(matrix[0].length).fill(0));
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let k = 0; k < matrix[i].length; k++) {
+      if (matrix[i][k] === true && k === 0) {
+        newMatrix[i][k + 1]++;
+      } else if (matrix[i][k] === true && k === matrix[i].length - 1) {
+        newMatrix[i][k - 1]++;
+      } else if (matrix[i][k] === true) {
+        newMatrix[i][k + 1]++;
+        newMatrix[i][k - 1]++;
+      }
+
+      if (matrix[i][k] === true && i === 0) {
+        newMatrix[i + 1][k]++;
+      } else if (matrix[i][k] === true && i === matrix.length - 1) {
+        newMatrix[i - 1][k]++;
+      } else if (matrix[i][k] === true) {
+        newMatrix[i + 1][k]++;
+        newMatrix[i - 1][k]++;
+      }
+
+      if (matrix[i][k] === true && i === 0 && k === 0) {
+        newMatrix[i + 1][k + 1]++;
+      } else if (
+        matrix[i][k] === true && i === 0 && k === matrix[i].length - 1) {
+        newMatrix[i + 1][k - 1]++;
+      } else if (
+        matrix[i][k] === true && i === matrix.length - 1 && k === matrix[i].length - 1) {
+        newMatrix[i - 1][k - 1]++;
+      } else if (matrix[i][k] === true && i === matrix.length - 1 && k === 0) {
+        newMatrix[i - 1][k + 1]++;
+      } else if (matrix[i][k] === true && k === 0) {
+        newMatrix[i - 1][k + 1]++;
+        newMatrix[i + 1][k + 1]++;
+      } else if (matrix[i][k] === true && k === matrix[i].length - 1) {
+        newMatrix[i - 1][k - 1]++;
+        newMatrix[i + 1][k - 1]++;
+      } else if (matrix[i][k] === true && i === 0) {
+        newMatrix[i + 1][k - 1]++;
+        newMatrix[i + 1][k + 1]++;
+      } else if (matrix[i][k] === true && i === matrix.length - 1) {
+        newMatrix[i - 1][k - 1]++;
+        newMatrix[i - 1][k + 1]++;
+      } else if (matrix[i][k] === true) {
+        newMatrix[i - 1][k - 1]++;
+        newMatrix[i - 1][k + 1]++;
+        newMatrix[i + 1][k + 1]++;
+        newMatrix[i + 1][k - 1]++;
+      }
+    }
+  }
+  return newMatrix;
 }
 
 module.exports = minesweeper;
